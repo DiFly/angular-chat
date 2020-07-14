eaee6a6b9cf896777704fb852a7f270a15f9d875
difly <kryshen.yaroslav@gmail.com>
2020-07-14 23:45:16 +0300

    add uuid lib, change user model

--- src/app/model/user.ts
+++ src/app/model/user.ts
@@ -1,2 +1,12 @@
+import { v1 as uuidv1 } from 'uuid';
+
 export class User {
+  id: string;
+
+  constructor(
+    public name: string,
+    public avatarSrc: string
+  ){
+    this.id = uuidv1();
+  }
 }
