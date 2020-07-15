daa07f186683197fe8ef4b7d705fed684df04953
difly <kryshen.yaroslav@gmail.com>
2020-07-15 23:06:45 +0300

    create thread model class

--- src/app/model/thread.ts
+++ src/app/model/thread.ts
@@ -1,2 +1,19 @@
+import { v1 as uuidv1 } from 'uuid';
+import { Message } from './message';
+
 export class Thread {
+  id: string;
+  lastMessage: Message;
+  name: string;
+  avatarSrc: string;
+
+  constructor(
+    id?: string,
+    name?: string,
+    avatarSrc?: string
+  ){
+    this.id = id || uuidv1();
+    this.name = name;
+    this.avatarSrc = avatarSrc;
+  }
 }
