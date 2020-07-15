e6c1d394228d399074dd89c3a764b2b89ae84704
difly <kryshen.yaroslav@gmail.com>
2020-07-15 23:13:42 +0300

    change model Message class

--- src/app/model/message.ts
+++ src/app/model/message.ts
@@ -1,2 +1,22 @@
+import { Thread } from './thread';
+import { User } from './user';
+import { v1 as uuidv1 } from 'uuid';
+
 export class Message {
+  id: string;
+  sentAt: Date;
+  isRead: boolean;
+  author: User;
+  text: string;
+  thread: Thread;
+
+  constructor( obj?: any ) {
+    this.id = obj && obj.id || uuidv1();
+    this.isRead = obj && obj.isRead || false;
+    this.sentAt = obj && obj.sentAt || new Date();
+    this.author = obj && obj.author || null;
+    this.text = obj && obj.text || null;
+    this.thread = obj && obj.thread || null;
+  }
+
 }
