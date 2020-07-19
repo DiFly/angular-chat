0bdd433a9002587ecb99a260f63984175aaa95e6
difly <kryshen.yaroslav@gmail.com>
2020-07-19 23:30:58 +0300

    create message.service test

--- src/app/service/messages.service.spec.ts
+++ src/app/service/messages.service.spec.ts
@@ -1,3 +1,6 @@
+import { Message } from './../model/message';
+import { Thread } from './../model/thread';
+import { User } from './../model/user';
 import { TestBed } from '@angular/core/testing';
 
 import { MessagesService } from './messages.service';
@@ -12,5 +15,34 @@ describe('MessagesService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should test', () => {
+    const user: User = new User('Kate', '');
+    const thread: Thread = new Thread('t1', 'Kate', '');
+    const m1: Message = new Message({
+      author: user,
+      text: 'Hi!',
+      thread: thread
+    });
+
+    const m2: Message = new Message({
+      author: user,
+      text: 'Bye!',
+      thread: thread
+    });
+
+    const messageService: MessagesService = new MessagesService();
+
+    messageService.newMessages.subscribe((message: Message) => {
+      console.log('=> newMessage: ' + message.text);
+    });
+
+    messageService.messages.subscribe((messages: Message[]) => {
+      console.log('=> messages: ' + messages.length);
+    });
+
+    messageService.addMessage(m1);
+    messageService.addMessage(m2);
   });
 });
