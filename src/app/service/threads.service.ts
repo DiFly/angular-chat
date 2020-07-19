b509b71e017eec74402fd9c42d33b9a9b77e5d7e
difly <kryshen.yaroslav@gmail.com>
2020-07-19 23:54:47 +0300

    change threads service

--- src/app/service/threads.service.ts
+++ src/app/service/threads.service.ts
@@ -1,9 +1,32 @@
+import { Message } from './../model/message';
+import { map } from 'rxjs/operators';
+import { MessagesService } from './messages.service';
+import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
+import { Thread } from '../model/thread';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ThreadsService {
+  threads: Observable<{ [key: string]: Thread }>;
 
-  constructor() { }
+  constructor(private messagesService: MessagesService) {
+    this.threads = this.messagesService.messages.pipe(
+      map( (messages: Message[]) => {
+        const threads: {[key: string]: Thread} = {};
+
+        messages.map((message: Message) => {
+          threads[message.thread.id] = threads[message.thread.id] || message.thread;
+
+          const messagesThread: Thread = threads[message.thread.id];
+          if(!messagesThread.lastMessage || messagesThread.lastMessage.sentAt < message.sentAt) {
+            messagesThread.lastMessage = message;
+          }
+        });
+
+        return threads;
+      })
+    );
+   }
 }
