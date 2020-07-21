75a682d37984fc661b41f40de152bdac2b8dbfa9
difly <kryshen.yaroslav@gmail.com>
2020-07-21 23:53:04 +0300

    add body to chat-thread component

--- src/app/component/chat-thread/chat-thread.component.ts
+++ src/app/component/chat-thread/chat-thread.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { ThreadsService } from './../../service/threads.service';
+import { Thread } from './../../model/thread';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-chat-thread',
@@ -6,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chat-thread.component.scss']
 })
 export class ChatThreadComponent implements OnInit {
+  @Input() thread: Thread;
+  selected = false;
 
-  constructor() { }
+  constructor(public threadService: ThreadsService) { }
 
   ngOnInit(): void {
+    this.threadService.currentThread.subscribe((currentThread: Thread) => {
+      this.selected = currentThread && this.thread && (currentThread.id === this.thread.id);
+    });
+  }
+
+  clicked(event: any): void {
+    this.threadService.setCurrentThread(this.thread);
+    event.preventDefault();
   }
 
 }
