46198987553a4ede826542580daf3039661d7c14
difly <kryshen.yaroslav@gmail.com>
2020-07-22 15:42:06 +0300

    add messages, and init class chatExampleData

--- src/app/data/chat-example-data.ts
+++ src/app/data/chat-example-data.ts
@@ -1,3 +1,8 @@
+import { UserService } from './../service/user.service';
+import { ThreadsService } from './../service/threads.service';
+import { MessagesService } from './../service/messages.service';
+import * as moment from 'moment';
+
 import { Message } from './../model/message';
 import { Thread } from './../model/thread';
 import { User } from './../model/user';
@@ -13,4 +18,54 @@ const tEcho: Thread = new Thread('tEcho', echo.name, echo.avatarSrc);
 const tRev: Thread = new Thread('tRev', rev.name, rev.avatarSrc);
 const tWait: Thread = new Thread('tWait', wait.name, wait.avatarSrc);
 
-const initialMessages: Array<Message> = []
+const initialMessages: Array<Message> = [
+  new Message({
+    author: me,
+    sentAt: moment().subtract(45, 'minutes').toDate(),
+    text: 'You bite up because of your lower jaw.',
+    thread: tLadycap,
+  }),
+  new Message({
+    author: ladycap,
+    sentAt: moment().subtract(20, 'minutes').toDate(),
+    text:
+      'Each person who knows you has a different perception of who you are.',
+    thread: tLadycap,
+  }),
+  new Message({
+    author: echo,
+    sentAt: moment().subtract(1, 'minutes').toDate(),
+    text:
+      "She could hear him in the shower singing with a joy she hoped he'd retain after she delivered the news.",
+    thread: tRev,
+  }),
+  new Message({
+    author: wait,
+    sentAt: moment().subtract(4, 'minutes').toDate(),
+    text: 'She saw the brake lights, but not in time.',
+    thread: tWait,
+  }),
+];
+
+export class ChatExampleData {
+    static init(
+        messagesService: MessagesService,
+        threadsService: ThreadsService,
+        usersService: UserService
+    ): void {
+        messagesService.messages.subscribe(() => ({}));
+        usersService.setCurrentUser(me);
+        initialMessages.map((message: Message) =>
+            messagesService.addMessage(message)
+        );
+        threadsService.setCurrentThread(tEcho);
+        this.setupBots(messagesService);
+    }
+
+    static setupBots(
+        messagesService: MessagesService
+    ): void {
+
+    };
+}
+
