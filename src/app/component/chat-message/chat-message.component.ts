8328119131dcb9fb9000505f30d15dea8101271e
difly <kryshen.yaroslav@gmail.com>
2020-07-28 23:17:46 +0300

    change chat-message component and template

--- src/app/component/chat-message/chat-message.component.ts
+++ src/app/component/chat-message/chat-message.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { UserService } from './../../service/user.service';
+import { User } from './../../model/user';
+import { Message } from './../../model/message';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-chat-message',
@@ -6,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chat-message.component.scss']
 })
 export class ChatMessageComponent implements OnInit {
+  @Input() message: Message;
+  currentUser: User;
+  incoming: boolean;
 
-  constructor() { }
+  constructor(
+    public userService: UserService
+  ) { }
 
   ngOnInit(): void {
+    this.userService.currentUser.subscribe(
+      (user: User) => {
+        this.currentUser = user;
+        if(this.message.author && user) {
+          this.incoming = this.message.author.id !== user.id;
+        }
+      }
+    );
   }
 
 }
